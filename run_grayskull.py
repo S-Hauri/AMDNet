c86ca84a344e23503f17feb0db5a062d857c4b12
sgbaird <sterling.baird@icloud.com>
2022-01-10 12:50:47 -0700

    comment utf-8 encoding conversion

--- run_grayskull.py
+++ run_grayskull.py
@@ -6,38 +6,40 @@ from souschef.recipe import Recipe
 
 import amdnet as module
 
+# def convert_file(srcfile, trgfile="tmp"):
+#     """How to convert a file to utf-8 in Python?
 
-def convert_file(srcfile, trgfile="tmp"):
-    if trgfile is None:
-        trgfile = srcfile
-    # get file encoding type
-    def get_encoding_type(file):
-        with open(file, "rb") as f:
-            rawdata = f.read()
-        return detect(rawdata)["encoding"]
+#     Modified from source: https://stackoverflow.com/a/53851783/13697228"""
+#     if trgfile is None:
+#         trgfile = srcfile
+#     # get file encoding type
+#     def get_encoding_type(file):
+#         with open(file, "rb") as f:
+#             rawdata = f.read()
+#         return detect(rawdata)["encoding"]
 
-    from_codec = get_encoding_type(srcfile)
+#     from_codec = get_encoding_type(srcfile)
 
-    # add try: except block for reliability
-    try:
-        with open(srcfile, "r", encoding=from_codec) as f, open(
-            trgfile, "w", encoding="utf-8"
-        ) as e:
-            text = f.read()  # for small files, for big use chunks
-            e.write(text)
+#     # add try: except block for reliability
+#     try:
+#         with open(srcfile, "r", encoding=from_codec) as f, open(
+#             trgfile, "w", encoding="utf-8"
+#         ) as e:
+#             text = f.read()  # for small files, for big use chunks
+#             e.write(text)
 
-        os.remove(srcfile)  # remove old encoding file
-        os.rename(trgfile, srcfile)  # rename new encoding
-    except UnicodeDecodeError:
-        print("Decode Error")
-    except UnicodeEncodeError:
-        print("Encode Error")
+#         os.remove(srcfile)  # remove old encoding file
+#         os.rename(trgfile, srcfile)  # rename new encoding
+#     except UnicodeDecodeError:
+#         print("Decode Error")
+#     except UnicodeEncodeError:
+#         print("Encode Error")
 
 
-files = os.popen("git ls-tree --full-tree -r --name-only HEAD").read().split("\n")
+# files = os.popen("git ls-tree --full-tree -r --name-only HEAD").read().split("\n")
 
-for file in files:
-    convert_file(file)
+# for file in files:
+#     convert_file(file)
 
 os.system("grayskull pypi {0}=={1}".format(module.__name__, module.__version__))
 
